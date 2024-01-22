b328550b1c4141a4969d9bdd938df846e941ca77
Santiago Servera <santiagoservera21@gmail.com>
2024-01-22 00:56:09 -0300

    Cambiando tarjetas de MainPage y aplicando Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const plugin = require ('tailwindcss/plugin')
+
+
+const rotationClass = plugin (function ({addUtilities}){
+  addUtilities({
+    ".my-rotate-y-180":{
+      transform: "rotateY(180deg)"
+    },
+    ".preserve-3d":{
+      transformStyle: "preserve-3d"
+    },
+    ".perspective":{
+      perspective: "1000px"
+    },
+    ".backface-hidden":{
+      backfaceVisibility: "hidden"
+
+    }
+  })
+});
+  
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [rotationClass],
 };
