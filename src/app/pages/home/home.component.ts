d6c448593ca9dfd955e9b7abe7b7267878a1ea89
Santiago Servera <santiagoservera21@gmail.com>
2024-01-26 11:56:33 -0300

    Services section component created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CardBoxComponent } from '../../components/card-box/card-box.component';
 import { CardContactComponent } from '../../components/card-contact/card-contact.component';
+import { ServicesComponent } from '../services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, CardBoxComponent, CardContactComponent],
+  imports: [NavbarComponent, CardBoxComponent, CardContactComponent,ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
