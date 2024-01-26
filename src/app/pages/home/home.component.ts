5d9271e214403bdac4b0167df0b3ea0ecb477b6c
Ruben Morales <rm96090@gmail.com>
2024-01-26 12:20:11 -0300

    Bug Fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CardBoxComponent } from '../../components/card-box/card-box.component';
 import { CardContactComponent } from '../../components/card-contact/card-contact.component';
 import { ServicesComponent } from '../services/services.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, CardBoxComponent, CardContactComponent,ServicesComponent],
+  imports: [NavbarComponent, CardBoxComponent, CardContactComponent,ServicesComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
