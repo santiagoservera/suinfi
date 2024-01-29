ed68d54279654520bffa3600f623130bcc41f122
Ruben Morales <rm96090@gmail.com>
2024-01-29 11:25:37 -0300

    Carousel bug fixes

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
-
+export class ProjectsComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    $(document).ready(function(){
+      $(".owl-carousel").owlCarousel({
+          center: true,
+          items:2,
+          loop:true,
+          nav: true,
+          margin:30,
+          responsive:{
+              992:{
+                  items:3
+              },
+          1200:{
+            items:4
+          }
+          }
+      });
+    });
+  }
 }
