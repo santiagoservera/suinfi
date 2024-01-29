82ac204421c8e3652fd7f040e0674afd9dd7bb4e
DanielCastillaa <danielcastillaa123@gmail.com>
2024-01-29 11:43:13 -0300

    Info component created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { CardBoxComponent } from '../../components/card-box/card-box.component';
 import { CardContactComponent } from '../../components/card-contact/card-contact.component';
 import { ServicesComponent } from '../services/services.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { InfoComponent } from '../../components/info/info.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, CardBoxComponent, CardContactComponent,ServicesComponent, ProjectsComponent],
+  imports: [NavbarComponent, CardBoxComponent, CardContactComponent,ServicesComponent, ProjectsComponent, InfoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
