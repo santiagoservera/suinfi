09cd8e744daaa9b035a944d7a61005f6600148a8
Santiago Servera <santiagoservera21@gmail.com>
2024-01-31 23:35:12 -0300

    Footer component separate to contact us component

--- src/app/components/footer-contact/footer-contact.component.ts
+++ src/app/components/footer-contact/footer-contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-footer-contact',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './footer-contact.component.html',
   styleUrl: './footer-contact.component.css'
 })
