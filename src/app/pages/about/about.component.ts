9cb28365d1cd6e41de78a3d3ffd66cb9abb252a9
DanielCastillaa <danielcastillaa123@gmail.com>
2024-02-05 13:28:04 -0300

    add WhatsApp chat to pages

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FooterContactComponent } from '../../components/footer-contact/footer-contact.component';
+import { ChatWspComponent } from '../../components/chat-wsp/chat-wsp.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent, FooterContactComponent],
+  imports: [NavbarComponent,FooterComponent, FooterContactComponent, ChatWspComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/pages/faqs/faqs.component.ts
+++ src/app/pages/faqs/faqs.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterContactComponent } from '../../components/footer-contact/footer-contact.component';
 import { FormContactComponent } from '../../components/form-contact/form-contact.component';
+import { ChatWspComponent } from '../../components/chat-wsp/chat-wsp.component';
 
 
 @Component({
   selector: 'app-faqs',
   standalone: true,
-  imports: [NavbarComponent, FooterContactComponent, FormContactComponent, CommonModule],
+  imports: [NavbarComponent, FooterContactComponent, FormContactComponent, CommonModule, ChatWspComponent],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.css'
 })
