160ebccccbbea9d43669c2cd1f83bd227f736f21
linearesdiego <diegolineares@gmail.com>
2024-02-12 13:59:38 -0300

    fix styles customers

--- src/app/components/softwareFactory/customers/customers.component.ts
+++ src/app/components/softwareFactory/customers/customers.component.ts
@@ -7,14 +7,14 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent implements AfterViewInit {
-  constructor() {}
+export class CustomersComponent /* implements AfterViewInit  */ {
+  constructor() { }
 
-  ngAfterViewInit(): void {
+  /* ngAfterViewInit(): void {
     var copy = document.querySelector('.customers-slide')?.cloneNode(true);
     var parent = document.querySelector(".customers");
     if (copy && parent) {
       parent.appendChild(copy);
     }
-  }
+  } */
 }
