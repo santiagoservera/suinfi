94b09d45ead604cdba48db53cc8f6fe39c69ea23
Ruben Morales <rm96090@gmail.com>
2024-02-13 16:33:03 -0300

    Customers SwiperJS

--- src/app/components/softwareFactory/customers/customers.component.ts
+++ src/app/components/softwareFactory/customers/customers.component.ts
@@ -1,20 +1,58 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SwiperContainer, register } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
+register();
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrl: './customers.component.css',
 })
-export class CustomersComponent /* implements AfterViewInit  */ {
-  constructor() { }
+export class CustomersComponent implements OnInit {
 
-  /* ngAfterViewInit(): void {
-    var copy = document.querySelector('.customers-slide')?.cloneNode(true);
-    var parent = document.querySelector(".customers");
-    if (copy && parent) {
-      parent.appendChild(copy);
-    }
-  } */
+  swiperElement = signal<SwiperContainer | null>(null);
+
+  ngOnInit(): void {
+    const swiperElemConstructor = document.querySelector('swiper-container');
+    const swiperOptions: SwiperOptions = {
+      slidesPerView: 7,
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+        1024: {
+          slidesPerView: 7,
+          spaceBetween: 60,
+        },
+      },
+      speed: 1000,
+      centeredSlides: true,
+      allowTouchMove: false,
+      loop: true,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+      },
+    };
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElement.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElement()?.initialize();
+  }
 }
