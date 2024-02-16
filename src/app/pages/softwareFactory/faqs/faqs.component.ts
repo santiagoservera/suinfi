99e00845908175a8cd99209864e07c2bc1b569eb
linearesdiego <diegolineares@gmail.com>
2024-02-15 23:17:45 -0300

    fixes form http

--- src/app/pages/softwareFactory/faqs/faqs.component.ts
+++ src/app/pages/softwareFactory/faqs/faqs.component.ts
@@ -4,13 +4,36 @@ import { NavbarComponent } from '../../../components/softwareFactory/navbar/navb
 import { FooterContactComponent } from '../../../components/softwareFactory/footer-contact/footer-contact.component';
 import { FormContactComponent } from '../../../components/softwareFactory/form-contact/form-contact.component';
 import { ChatWspComponent } from '../../../components/softwareFactory/chat-wsp/chat-wsp.component';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormContactService } from '../../../services/form-contact.service';
 
 
 @Component({
   selector: 'app-faqs',
   standalone: true,
-  imports: [NavbarComponent, FooterContactComponent, FormContactComponent, CommonModule, ChatWspComponent],
+  imports: [NavbarComponent, FooterContactComponent, FormContactComponent, CommonModule, ChatWspComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.css'
 })
-export class FaqsComponent {}
+export class FaqsComponent {
+  constructor(private service: FormContactService) { }
+
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    surName: new FormControl(''),
+    email: new FormControl(''),
+    subject: new FormControl(''),
+    message: new FormControl('')
+  });
+
+  send() {
+    this.service.sendData(this.contactForm.value).subscribe((response) => {
+      try {
+        console.log(response);
+        this.contactForm.reset();
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  }
+}
