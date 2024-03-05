d809438fd07f2e100b14074bd864532f38a40b66
Ruben Morales <rm96090@gmail.com>
2024-03-05 17:15:01 -0300

    Auth design

--- src/app/pages/ecommerce/auth/auth.component.ts
+++ src/app/pages/ecommerce/auth/auth.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
